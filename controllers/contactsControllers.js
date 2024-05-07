f687c3ebde51b19a0e1c7f111c40553e77369d8d
Aleksandr-Reznichenko <reznichenko.aleksandr.v@gmail.com>
2024-05-07 22:41:28 +0300

    hw-03

--- controllers/contactsControllers.js
+++ controllers/contactsControllers.js
@@ -4,6 +4,7 @@ import {
   removeContact,
   addContact,
   updateContact as updateContactService,
+  updateStatusContact,
 } from "../services/contactsServices.js";
 import HttpError from "../helpers/HttpError.js";
 
@@ -42,11 +43,7 @@ export const deleteContact = async (req, res, next) => {
 
 export const createContact = async (req, res, next) => {
   try {
-    const newContact = await addContact(
-      req.body.name,
-      req.body.email,
-      req.body.phone
-    );
+    const newContact = await addContact(req.body);
     res.status(201).json(newContact);
   } catch (error) {
     next(HttpError(500, "Failed to create contact"));
@@ -56,6 +53,21 @@ export const createContact = async (req, res, next) => {
 export const updateContact = async (req, res, next) => {
   try {
     const updatedContact = await updateContactService(req.params.id, req.body);
+    if (!updatedContact) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(200).json(updatedContact);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateFavorite = async (req, res, next) => {
+  try {
+    const updatedContact = await updateStatusContact(
+      req.params.id,
+      req.body.favorite
+    );
     if (!updatedContact) {
       throw HttpError(404, "Not found");
     }
--- routes/contactsRouter.js
+++ routes/contactsRouter.js
@@ -5,23 +5,26 @@ import {
   getAllContacts,
   getOneContact,
   updateContact,
+  updateFavorite,
 } from "../controllers/contactsControllers.js";
 import validateBody from "../helpers/validateBody.js";
 import {
   createContactSchema,
   updateContactSchema,
+  updateFavoriteSchema,
 } from "../schemas/contactsSchemas.js";
 
 const contactsRouter = express.Router();
 
 contactsRouter.get("/", getAllContacts);
-
 contactsRouter.get("/:id", getOneContact);
-
 contactsRouter.delete("/:id", deleteContact);
-
 contactsRouter.post("/", validateBody(createContactSchema), createContact);
-
 contactsRouter.put("/:id", validateBody(updateContactSchema), updateContact);
+contactsRouter.patch(
+  "/:id/favorite",
+  validateBody(updateFavoriteSchema),
+  updateFavorite
+);
 
 export default contactsRouter;
